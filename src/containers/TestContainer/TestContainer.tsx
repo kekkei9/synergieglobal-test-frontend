0af723b370417628285438da038147b67b28ed66
kekkei9 <kekkei2002@gmail.com>
2024-10-25 23:56:20 +0700

    setting up folder structure & adding typescript and tailwindcss

---
+++ src/containers/TestContainer/TestContainer.tsx
@@ -0,0 +1,12 @@
+import TestComponent from "@/components/TestComponent/TestComponent";
+import React from "react";
+
+const TestContainer = () => {
+  return (
+    <div>
+      <TestComponent />
+    </div>
+  );
+};
+
+export default TestContainer;
